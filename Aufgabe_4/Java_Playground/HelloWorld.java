7730fc08680452c88f5fbd1251c4e60d6c895a3d
Lord-Leonard <leonardgeorg@gmx.de>
2021-11-02 11:16:09 +0100

    Aufgabe 3: Initial Commit Aufgabe 4: Initial Commit

---
+++ Aufgabe_4/Java_Playground/HelloWorld.java
@@ -0,0 +1,11 @@
+package Aufgabe_4.Java_Playground;
+
+public class HelloWorld 
+{
+      public static void main (String[] args)
+      {
+            int i = 0;
+            System.out.println(i);
+
+      }
+}
